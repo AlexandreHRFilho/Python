4031ff4faf1701b5f73540872c9a392640398910
Alexandre Henrique <alexandrehrfilho@gmail.com>
2022-03-19 21:29:43 -0300

    Update: new function Delete to menu
    Allows you to delete a row. We create a temporary list to copy all the rows,  ask what row wanna delete , after its possible to copy to csv the lines remaining in a new csv file.

--- subprograms/salaries.py
+++ subprograms/salaries.py
@@ -1,5 +1,6 @@
 import csv
 
+
 def addtofile():
     file = open("Salaries.csv","a")
     name = input("Enter your name: ")
@@ -12,10 +13,28 @@ def viewRecords():
     for x in file:
         print(x)
     file.close()
+def deleteRowRecord():
+    file = open("Salaries.csv","r")
+    tmp_file = []
+    for row in file:
+        tmp_file.append(row)
+    file.close()
+    x=0 # Lets possible see the line that user want to delete
+    for row in tmp_file:
+        print(x,row)
+        x +=1
+    rowTodelete = int(input("Enter the row number to delete: "))
+    del tmp_file[rowTodelete]
+    file = open("Salaries.csv","w")
+    for row in tmp_file:
+        file.write(row)
+    file.close()
+    
+
 
 again = True
 while again:
-    print("\n1- Add to file\n2-View Records\n3-Close the program")
+    print("\n1- Add to file\n2-View Records\n3-Delete a record\n4-Close the program")
     selection = int(input("Enter the number of your selection: "))
     print("\n")
     if selection == 1:
@@ -23,6 +42,8 @@ while again:
     elif selection == 2:
         viewRecords()
     elif selection == 3:
+        deleteRowRecord()
+    elif selection == 4:
         again = False
     else:
         print("Invalid option")
