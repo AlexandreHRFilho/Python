3e862070c602f9567d70298262b336538f77b735
Alexandre Henrique <alexandrehrfilho@gmail.com>
2022-03-21 18:11:28 -0300

    Csv file to save List  with tkinter
    This algorithm makes possible to add itens in your list and them save in a file  after some changes.

--- tkinter/is_digital.py
+++ tkinter/is_digital.py
@@ -1,4 +1,14 @@
 from tkinter import *
+import csv
+def csvSave():
+    file = open("saveFile.csv","w")
+    tmp_list = num_list.get(0,END)
+    item = 0
+    for x in tmp_list:
+        newrecord = tmp_list[item] + "\n"
+        file.write(str(newrecord))
+        item += 1
+    file.close()
 def add_num():
     num = entry_box.get()
     if num.isdigit():
@@ -24,4 +34,6 @@ buttonClean.place(x=70,y=10,width=35,height=25)
 num_list = Listbox()
 num_list.place(x=100,y=50,width=100,height=150)
 num_list["justify"] = "center"
+buttonSave = Button(text="Save",command= csvSave)
+buttonSave.place(x=235,y=10,width=25,height=25)
 window.mainloop()
